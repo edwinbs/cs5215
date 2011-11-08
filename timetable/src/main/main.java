38ba9a461b0cec0b87a5cf4136d1ee3853a8555e
Edwin Boaz Soenaryo <edwinboaz@gmail.com>
2011-11-08 22:36:07 +0800

    Added run instructions.

--- timetable/src/main/main.java
+++ timetable/src/main/main.java
@@ -4,7 +4,12 @@ public class main {
 
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.printf("Usage: java -jar Timetable.jar <input-file>\n");
+            System.out.printf("Usage: java main.main <input-file> [options]\n");
+            System.out.printf("Options:\n");
+            System.out.printf(" -s --seed <seed-value>\n");
+            System.out.printf("     Use <seed-value> as the random number generator seed.\n");
+            System.out.printf(" -t --timeout <timeout-secs>\n");
+            System.out.printf("     Terminate the solver after <timepit-secs> seconds. Default=375s.\n");
             System.exit(1);
         }
 
